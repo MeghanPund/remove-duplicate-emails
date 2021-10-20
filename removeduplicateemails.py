222989e1125ca210cb38315f5abb823091b1ac87
Meghan Pund <meghan.pund@gmail.com>
2021-10-20 15:09:39 -0400

    I heart python

--- removeduplicateemails.py
+++ removeduplicateemails.py
@@ -29,13 +29,16 @@ user_input = input("Add a new email or enter q to quit: ").lower().strip()
 
 # accept new emails as input !! ADD REGEX TO CHECK EMAIL FORMAT !!
 def get_new_email():
+    global user_input
     if user_input not in new_list:
         new_list.append(user_input)
         print("You just added: " + user_input)
-        return
+        user_input = input("Add another new email or enter q to quit: ").lower().strip()
+        return        
     elif user_input in new_list:
         print("Sorry. This email already exists.")
-        return
+        user_input = input("Add another new email or enter q to quit: ").lower().strip()
+
 
 # master loop to add new emails to email list
 while adding_emails:
@@ -44,10 +47,10 @@ while adding_emails:
         break
     elif user_input != "q" or "quit":
         get_new_email()
-        break
+        continue
     else:
         print("Invalid input.")
-        break
+        continue
 
 new_list = sorted(list(set(new_list)))
 
@@ -61,4 +64,4 @@ print("There are " + str(len(new_list)) + " unique emails in your list." )
 updated_email_list = open('updatedjazzhangsemails.txt', 'w')
 
 # sort(alphabetize) list and format for use in gmail, then write new list to file
-updated_email_list.write(str(sorted(new_list)).replace(",", ";").replace("'", ""))
+updated_email_list.write(str(new_list).replace(",", ";").replace("'", ""))
